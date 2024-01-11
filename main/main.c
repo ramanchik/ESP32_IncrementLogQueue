0e24f04f50445368b8e892de66f9ba1ae6352eb1
ramanchik <ramanchik@gmail.com>
2024-01-11 17:10:42 +0300

    Add a check for the correct start of the program and different stopping behavior.

--- main/main.c
+++ main/main.c
@@ -11,6 +11,10 @@
 
 #define INCREMENT_SENDING_PERIOD_MILLIS		5000
 
+#define QUEUE_LENGTH	100
+
+#define RESTART_IF_STOPPED
+
 
 typedef struct {
 	uint32_t value;
@@ -35,6 +39,7 @@ void task_incrementer(void *pvParameter)
 {
 	uint32_t value = 0;
 	TickType_t previousTicksValue = xTaskGetTickCount();
+    TickType_t lastWakeTime = xTaskGetTickCount();
 
 	while(1){
 		value++;
@@ -45,8 +50,6 @@ void task_incrementer(void *pvParameter)
 		xQueueSend(queue, &message, 0);
 
 		vTaskDelayUntil(&lastWakeTime, INCREMENT_SENDING_PERIOD_MILLIS / portTICK_PERIOD_MS);
-
-
 	}
 }
 
@@ -77,10 +80,40 @@ void task_logger(void *pvParameter)
 }
 
 
+void system_stop() {
+	ESP_LOGE(TAG, "Stopped!");
+
+#ifdef RESTART_IF_STOPPED
+	ESP_LOGE(TAG, "The device will be restarting in a 3 seconds...");
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+	ESP_LOGE(TAG, "Restarting now.");
+    fflush(stdout);
+	esp_restart();
+#endif
+}
+
+
+
 void app_main(void)
 {
-	queue = xQueueCreate(10, sizeof(IncrementMessage));
-	xTaskCreate(&task_logger, "task_logger", 2048, NULL, 5, NULL);
-    xTaskCreate(&task_incrementer, "task_incrementer", 2048, NULL, 5, NULL);
+	queue = xQueueCreate(QUEUE_LENGTH, sizeof(IncrementMessage));
+    if(queue == NULL) {
+    	ESP_LOGE(TAG, "Not enough memory to allocate for the queue. Try to reduce queue length.");
+		system_stop();
+		return;
+    }
 
+    BaseType_t loggerTaskResult = xTaskCreate(&task_logger, "task_logger", 2048, NULL, 5, NULL);
+    if(loggerTaskResult == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY){
+    	ESP_LOGE(TAG, "Not enough memory to allocate for the task 'logger'.");
+		system_stop();
+		return;
+    }
+
+    BaseType_t incrementerTaskResult = xTaskCreate(&task_incrementer, "task_incrementer", 2048, NULL, 5, NULL);
+    if(incrementerTaskResult == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY){
+    	ESP_LOGE(TAG, "Not enough memory to allocate for the task 'incrementer'.");
+		system_stop();
+		return;
+    }
 }
